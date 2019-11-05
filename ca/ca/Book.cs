af3266f3fb8abf8e40ecb9fdb3a39215f3d3c987
s <s@z>
2019-11-05 16:23:21 +0000

    hey

---
+++ ca/ca/Book.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ca
+{
+
+    enum Genre
+    {
+        Fiction,
+        Biography,
+        Computing,
+        Literature,
+    }
+
+
+    class Book : IComparable<Book>
+    {
+        #region properties
+
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public DateTime Published { get; private set; }
+        public int Pages { get; private set; }
+        public Genre Genre { get; private set; }
+
+        #endregion properties
+
+        #region constructors
+
+        public Book(String author, string title, Genre genre, DateTime published, int pages)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Published = published;
+            Pages = pages;
+        }
+
+        public Book(String author, string title, Genre genre, DateTime published )
+            :this ( author, title, genre, published, 0 )
+        {
+
+        }
+
+        public Book(String author, string title)
+            :this(author, title, Genre.Fiction, DateTime.Now )
+        {
+
+        }
+
+
+        #endregion constructors
+
+
+        public override string ToString()
+        {
+            string s1 = "{0,-30}{1,-30}{2,-10}{3,-12}{4,-20}";
+            return string.Format(s1, Author, Title, Pages, Genre, Published.ToShortDateString() );
+        }
+
+        public int CompareTo(Book that)
+        {
+            if( this.Author == that.Author )
+            {
+                bool b1 = string.Compare(this.Title, that.Title) < 0;
+                if (b1)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                bool b1 = string.Compare(this.Author, that.Author) < 0;
+                if (b1)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+}
--- ca/ca/Program.cs
+++ ca/ca/Program.cs
@@ -1,17 +1,73 @@
 ﻿using System;
 
+using System.Collections.Generic;
+
 namespace ca
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Book> ReadingList = new List<Book>();
+
+            ReadingList.Add(new Book("James Joyce", "Ulysses", Genre.Fiction, new DateTime(1922, 2, 2), 730 ));
+            ReadingList.Add(new Book( "B Kernighan and D Ritchie","The C Programming Language", Genre.Fiction, new DateTime(1978, 2, 28), 279 ));
+            ReadingList.Add(new Book( "Walter Isaacson","Steve Jobs", Genre.Fiction, new DateTime(2011, 10, 24), 627 ));
+            ReadingList.Add(new Book( "JRR Tolkien","The Hobbit", Genre.Fiction, new DateTime(1937, 9, 21), 310 ));
+            ReadingList.Add(new Book("James Joyce", "Portrait of the Artist", Genre.Fiction, new DateTime(1916, 12, 29), 299 ));
+
+            DisplayBooks(ReadingList);
+            ReadingList.Sort();
+            Console.WriteLine();
+            DisplayBooks(ReadingList);
+            Console.WriteLine();
+            DisplayBooksAndAge(ReadingList);
         }
-    }
 
-    class Book
-    {
 
+        static void DisplayBooks(List<Book> ReadingList)
+        {
+            string s1 = "{0,-30}{1,-30}{2,-10}{3,-12}{4,-20}";
+            Console.WriteLine(s1, "Author", "Title", "Pages", "Genre", "Publication Date");
+            foreach (Book b in ReadingList)
+            {
+                Console.WriteLine(b.ToString());
+            }
+        }
+
+
+        static void DisplayBooksAndAge(List<Book> ReadingList)
+        {
+            string s1 = "{0,-30}{1,-30}{2,-10}{3,-12}{4,-20}{5,-6}";
+            Console.WriteLine(s1, "Author", "Title", "Pages", "Genre", "Publication Date", "Age");
+            foreach (Book b in ReadingList)
+            {
+                Console.WriteLine("{0, -102}{1}", b.ToString(), CalculateAge(b.Published) );
+            }
+        }
+
+
+        // adapted from
+        // https://stackoverflow.com/questions/4127363/date-difference-in-years-using-c-sharp
+        static int CalculateAge(DateTime datetime)
+        {
+            DateTime zeroTime = new DateTime(1, 1, 1);
+            DateTime d2 = DateTime.Now;
+
+            TimeSpan span = d2.Subtract(datetime);
+            int years = (zeroTime + span).Year - 1;
+
+            return years;
+        }
+
+
+        // not used
+        static int CalculateAge2(DateTime datetime)
+        {
+            DateTime d2 = DateTime.Now;
+            TimeSpan timespan = d2.Subtract(datetime);
+            double y1 = (timespan.Days / 365.25);
+            return Convert.ToInt32(Math.Truncate(y1));
+        }
     }
 }
